9dff44f155a963a96cd2d4cfe3d3e4689e8a5ac6
Matthew <148456106+Irpacci@users.noreply.github.com>
2024-05-04 14:44:54 +0000

    cards now return to hand 04/05/24

--- scenes/card_ui/card_states/card_released_state.gd
+++ scenes/card_ui/card_states/card_released_state.gd
@@ -1,5 +1,18 @@
 extends CardState
 
+var played: bool
+
 func enter() -> void:
 	card_ui.color.color = Color.DARK_MAGENTA
 	card_ui.state.text = "Released"
+	played = false
+	if not card_ui.targets.is_empty():
+		played = true
+		print("card_played ", card_ui.targets)
+		
+func on_input(_event: InputEvent) -> void:
+	if played:
+		return
+	transition_requested.emit(self, CardState.State.BASE)
+
+
