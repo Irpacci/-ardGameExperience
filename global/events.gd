517646e5357c63fb023fab07890638de7363af27
Matthew <148456106+Irpacci@users.noreply.github.com>
2024-05-13 11:10:31 +0000

    added enemy ui

--- global/events.gd
+++ global/events.gd
@@ -1,5 +1,6 @@
 extends Node
 
+#cards
 signal card_drag_started(card_ui: CardUI) 
 signal card_drag_ended(card_ui: CardUI)
 signal card_aim_started(card_ui: CardUI)
@@ -8,6 +9,12 @@ signal card_tooltip_requested(card: Card)
 signal tooltip_hide_requested
 signal card_played(card: Card)
 
+#player
 signal player_hand_drawn
 signal player_hand_discarded
 signal player_turn_ended
+signal player_died
+
+#enemy
+signal enemy_action_completed(enemy: Enemy)
+signal enemy_turn_ended
